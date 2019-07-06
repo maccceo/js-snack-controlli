683a20c64edb7fbcff09355f8557b6d560efbc27
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-06 18:08:27 +0200

    jSnack5 - Esercizio completato

--- jSnack5/main.js
+++ jSnack5/main.js
@@ -1,2 +1,13 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array
+
+var array = [], userInput;
+
+for (var i = 0; i < 6; i++) {
+	userInput = prompt ("Inserisci un numero:");
+	if (userInput%2 == 1) {
+		array.push(userInput);
+	}
+}
+
+console.log('hai inserito i seguenti numeri dispari: ' + array);
