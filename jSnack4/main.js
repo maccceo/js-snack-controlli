88610a1bd7b2399144e012671109b99b0c9294ff
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-06 17:36:45 +0200

    jSnack4 - definizione lista invitati e step procedurali

---
+++ jSnack4/main.js
@@ -0,0 +1,25 @@
+// In un array sono contenuti i nomi degli invitati alla festa del
+// grande Gatsby, chiedi all’utente il suo nome e comunicagli se
+// può partecipare o no alla festa.
+
+console.log('there i go');
+
+var guest = [
+	"Liberatore Costantini",
+	"Bruno Aiolfi",
+	"Settimio Donati",
+	"Luigi Leccese",
+	"Eulalia Croce",
+	"Crocetta Bassi",
+	"Attilio Antonini",
+	"Eugenio Perugia",
+	"Eloisa Alessi",
+	"Lino Benetton",
+	"Demetra Selvaggio"
+];
+
+//chiedi il nome all'utente
+
+//controlla se è in lista
+
+//comunica se può partecipare
