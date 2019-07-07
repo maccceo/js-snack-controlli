20f332268ba8910fd5a06bb3b6ea754be42709c0
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-07 18:19:46 +0200

    jSnack5pro - controlli input per avere soltanto numeri, refactoring

--- jSnack5pro/main.js
+++ jSnack5pro/main.js
@@ -1,13 +1,37 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array
 
-var array = [], userInput;
+// PUNTI CRITICI
+// - Assicurarsi che l'input sia accettabile per il mio script
+//		-> Ha inserito qualcosa?
+//		-> non accettare nulla al di fuori di numeri
+// in entrambi i casi, fai ripetere l'inserimento per mantenere i 6 numeri richiesti dall'esercizio
+
+var array = [], userInput, allClear = false;
 
 for (var i = 0; i < 6; i++) {
 	userInput = prompt ("Inserisci un numero:");
+	
+	// controlli input
+	while (allClear == false) {
+		// se l'utente non ha messo niente
+		if (!userInput) {
+			userInput = prompt ("Non hai inserito nulla, inserisci un numero:");
+		}
+		// se l'utente ha messo qualcosa al di fuori di un numero
+		else if (isNaN(userInput)) {
+			userInput = prompt ("Puoi inserire soltanto un numero:");
+		}
+		else {
+			allClear = true;
+		}
+	}
+	allClear = false;
+	console.log(userInput);
+
 	if (userInput%2 == 1) {
 		array.push(userInput);
 	}
 }
 
-console.log('hai inserito i seguenti numeri dispari: ' + array);
+alert('hai inserito i seguenti numeri dispari: ' + array);
